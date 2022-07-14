7687dccabbbe8721b96f6285b34d43dc1900a399
Aditya Raj Sharma <aditya@Adityas-MacBook-Pro.local>
2022-07-15 01:32:39 +0530

    components created and added

--- src/components/main.js
+++ src/components/main.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 const Main = () => {
+  return (
+    <>
+      <div id="Intro" >
+        <p>
+          Hi! My name is Aditya and I am a High School student 
+          studying in Bangalore. I am a JavaScript, Python, and 
+          React enthusiast and love playing basketball.
+        </p>
+      </div>
+    </>
+  ) 
 }
 
 export default Main;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,8 +3,10 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <div>
-        Navbar
+      <div className="navbar-container">
+        <h3 className="navbar-element">Intro</h3>
+        <h3 className="navbar-element">Projects</h3>
+        <h3 className="navbar-element">Contact</h3>
       </div>
     </>
   )
